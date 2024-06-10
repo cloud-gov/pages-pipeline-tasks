a0d75c5768cee6e905df851bf8549d739e4d5496
Drew Bollinger <drew.bollinger@gsa.gov>
2024-06-10 11:35:48 -0700

    add resource type templates, init task

---
+++ scripts/init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# install ytt
+ytt_checksum=357ec754446b1eda29dd529e088f617e85809726c686598ab03cfc1c79f43b56
+binary_type=linux-amd64
+dst_dir=/usr/local/bin
+
+echo "Installing ytt..."
+wget -nv -O- https://github.com/carvel-dev/ytt/releases/download/v0.49.0/ytt-${binary_type} > /tmp/ytt
+echo "${ytt_checksum}  /tmp/ytt" | shasum -c -
+mv /tmp/ytt ${dst_dir}/ytt
+chmod +x ${dst_dir}/ytt
+echo "Installed ${dst_dir}/ytt v0.49.0"
+
+# hydrate our pipelines with ytt overlays
+# https://carvel.dev/ytt/docs/v0.49.x/lang-ref-ytt-overlay
+
+ytt -f $PIPELINE_YML -f pipeline-tasks/resource-type-templates > src/set-pipeline.yml
+
+# now we can set_pipeline with that final file
